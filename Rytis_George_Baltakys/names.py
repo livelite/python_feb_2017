723073744475782600f708d8b4039345b36d132c
Rytis George <rytis@liteso.com>
2017-02-06 22:18:30 -0600

    Added last python fundamentals assigment

---
+++ Rytis_George_Baltakys/names.py
@@ -0,0 +1,74 @@
+'''
+Assignment: Names
+
+Part I:
+
+Given the following list:
+'''
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+'''
+Copy
+Create a program that outputs:
+
+Michael Jordan
+John Rosales
+Mark Guillen
+KB Tonel
+'''
+for student in students:
+	print student['first_name'], student['last_name']
+
+'''
+Part II:
+
+Now, given the following dictionary:
+'''
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+'''
+Create a program that prints the following format (including number of characters in each combined name):
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+Copy
+Create a program that prints the following format (including number of characters in each combined name):
+
+Students
+1 - MICHAEL JORDAN - 13
+2 - JOHN ROSALES - 11
+3 - MARK GUILLEN - 11
+4 - KB TONEL - 7
+Instructors
+1 - MICHAEL CHOI - 11
+2 - MARTIN PURYEAR - 13
+'''
+print '\n'
+for key,data in users.iteritems():
+	print key
+	i = 1
+	for d in data:
+		print '{} - {} {} - {}'.format(i, d['first_name'].upper(), d['last_name'].upper(), len(d['first_name']+d['last_name']))
